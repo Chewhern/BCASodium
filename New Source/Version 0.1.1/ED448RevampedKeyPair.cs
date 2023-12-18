d35d510360688123557455f4756fda8cb130699e
Chrono <chrono_legionnaire@protonmail.com>
2023-12-18 13:52:20 +0800

    Add files via upload

---
+++ New Source/Version 0.1.1/ED448RevampedKeyPair.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using ASodium;
+
+namespace BCASodium
+{
+    public class ED448RevampedKeyPair
+    {
+        private readonly byte[] _publicKey;
+        private readonly byte[] _privateKey;
+
+        public ED448RevampedKeyPair(byte[] publicKey, byte[] privateKey)
+        {
+            //verify that the private key length is exactly 57 bytes
+            if (privateKey.Length != 57)
+                throw new ArgumentException("Private Key length must be 57 bytes.");
+
+            _publicKey = publicKey;
+
+            _privateKey = privateKey;
+        }
+
+        ~ED448RevampedKeyPair()
+        {
+            Clear();
+        }
+
+        /// <summary>Gets the Public Key.</summary>
+        public byte[] PublicKey
+        {
+            get { return _publicKey; }
+        }
+
+        /// <summary>Gets the Private Key.</summary>
+        public byte[] PrivateKey
+        {
+            get
+            {
+                return _privateKey;
+            }
+        }
+
+        /// <summary>Clear private key and public key through cryptographically secure way.</summary>
+        public void Clear()
+        {
+            SodiumSecureMemory.SecureClearBytes(_publicKey);
+            SodiumSecureMemory.SecureClearBytes(_privateKey);
+        }
+    }
+}
